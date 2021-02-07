d61d0ff08772061b2d7832db2e66ad7a01c14ac4
Vincent-Projects <crysyroodpro@outlook.com>
2021-02-07 02:46:18 +0100

    07 Feb 2021 | Prime Number Exercice

--- src/21-02-07/src/main.cpp
+++ src/21-02-07/src/main.cpp
@@ -4,16 +4,52 @@
 
 using namespace std;
 
+bool isPrime(int number)
+{
+    if (number <= 0)
+    {
+        return false;
+    }
+
+    for (int i = 1; i < number; i++)
+    {
+        if ((number % i) == 0 && i != 1)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 int main()
 {
-    string input;
-    int age;
+    int test1 = 2;
+    int test2(11);
+    int test3 = 35;
+    int test4 = 47;
+    int test5{40};
+    int test6(85);
+    int test7 = 89;
+    int test8 = 8;
 
-    cout << "What is your age ?\n";
+    int isTest1Prime = isPrime(test1);
+    int isTest2Prime = isPrime(test2);
+    int isTest3Prime = isPrime(test3);
+    int isTest4Prime = isPrime(test4);
+    int isTest5Prime = isPrime(test5);
+    int isTest6Prime = isPrime(test6);
+    int isTest7Prime = isPrime(test7);
+    int isTest8Prime = isPrime(test8);
 
-    getline(cin, input);
-    stringstream(input) >> age;
+    cout << isTest1Prime << endl; // true
+    cout << isTest2Prime << endl; // true
+    cout << isTest3Prime << endl; // false
+    cout << isTest4Prime << endl; // true
+    cout << isTest5Prime << endl; // false
+    cout << isTest6Prime << endl; // false
+    cout << isTest7Prime << endl; // true
+    cout << isTest8Prime << endl; // false
 
-    cout << "Your age is " << age << endl;
     return 1;
 }
